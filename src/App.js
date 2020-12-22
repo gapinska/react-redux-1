dc4e5778945cef5c1be88f392ae6382197c1014d
Monika Gapińska <gapinska.mon@gmail.com>
2020-12-22 11:18:16 +0100

    useSelector Hook

--- src/App.js
+++ src/App.js
@@ -3,11 +3,14 @@ import './App.css'
 import { Provider } from 'react-redux'
 import CakeContainer from './components/CakeContainer'
 import store from './store/store'
+import HooksCakeContsainer from './components/HooksCakeContsainer'
+
 function App() {
 	return (
 		<Provider store={store}>
 			<div className="App">
 				<CakeContainer />
+				<HooksCakeContsainer />
 			</div>
 		</Provider>
 	)
